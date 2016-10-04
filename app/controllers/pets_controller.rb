85926795fcc013800ec9b71e47cca2859aca147f
Andrew <andrew.vinocur@gmail.com>
2016-10-04 10:48:54 -0700

    adds new and create action to controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,10 +12,25 @@ class PetsController < ApplicationController
     @owner = Owner.find_by(id: owner_id)
   end
 
-  # TODO: set up *new* method with data for new view
+  def new
+    @pet = Pet.new
+    owner_id = params[:owner_id]
+    @owner = Owner.find_by(id: owner_id)
+  end
 
-  # TODO: set up *create* method with database interactions for create
-  # TODO: handle save errors and communicate issues to user
+  def create
+    owner = Owner.find(params[:owner_id])
+    new_pet = Pet.new(pet_params)
+    if new_pet.save
+      owner.pets << new_pet
+      redirect_to owner_pet_path(owner, new_pet)
+    else
+      flash[:error] = new_pet.errors.full_messages.join(" ")
+      redirect_to new_owner_pet_path(owner)
+    end
+  end
+
+  end
 
   private
   def pet_params
